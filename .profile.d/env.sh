2743311f3c0d21ce7ae2eba63b8786fa89f47781
Anderthan Hsieh <anderthan@gmail.com>
2014-04-04 15:50:54 -0700

    updated paths

--- .profile.d/env.sh
+++ .profile.d/env.sh
@@ -1,1 +1,4 @@
+export PATH=/app/vendor/ruby-2.0.0/bin:$PATH
+export RUBYLIB=/app/vendor/ruby-2.0.0/lib/ruby/2.0.0/:/app/vendor/ruby-2.0.0/lib/ruby/2.0.0/x86_64-linux/
+export GEM_PATH=/app/vendor/ruby-2.0.0/lib/ruby/gems/2.0.0/
 export LD_LIBRARY_PATH=/app/vendor/openssl/lib/
